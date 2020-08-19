0c9f49d702300f211550e1391bfe58339f0abe2a
星野龍 <>
2020-08-19 13:50:31 +0900

    React-counterApp

--- src/App.js
+++ src/App.js
@@ -1,26 +1,50 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import { Button } from '@material-ui/core/';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const button = {
+	margin: "20px"
+}
+
+class App extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			counter: 0
+		}
+		this.countUp = this.countUp.bind(this);
+		this.countDown = this.countDown.bind(this);
+		this.countReset = this.countReset.bind(this);
+	}
+
+	countUp(){
+		this.setState({
+			counter: this.state.counter + 1,
+		})
+	}
+
+	countDown(){
+		this.setState({
+			counter: this.state.counter - 1,
+		})
+	}
+
+	countReset(){
+		this.setState({
+			counter: 0,
+		})
+	}
+
+	render() {
+		return (
+			<div className="container">
+				<h1>Counter: {this.state.counter}</h1>
+				<Button style={button} variant="contained" color="secondary" onClick={this.countUp}>INCREMENT</Button>
+				<Button style={button} variant="contained" color="primary" onClick={this.countDown}>DECREMENT</Button>
+				<Button style={button} variant="contained" onClick={this.countReset}>RESET</Button>
+			</div>
+		)
+	}
 }
 
 export default App;
